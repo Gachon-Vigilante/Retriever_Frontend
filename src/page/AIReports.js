4082699317b6e507b8f3dc6f32cf7221fb19f27d
zsjon <cho010105@naver.com>
2025-07-19 23:55:27 +0900

    [FIX] 툴팁박스 설명 문구 수정 1. 채널/리포트/게시글/그래프 툴팁박스 설명 문구 수정

--- src/page/AIReports.js
+++ src/page/AIReports.js
@@ -2,11 +2,12 @@ import Sidebar from "../components/Sidebar";
 import "../css/page/AiChat.css";
 import useFetchChannels from "../hooks/useFetchChannels";
 import React, {useEffect, useState} from "react";
-import { useSearchParams } from "react-router-dom";
+import {useSearchParams} from "react-router-dom";
 import ReactPaginate from "react-paginate";
 import "../css/components/Pagination.css";
 import axios from "axios";
 import ToolTip from "../components/ToolTip";
+
 axios.defaults.withCredentials = true;
 
 const AIReports = () => {
@@ -31,27 +32,27 @@ const AIReports = () => {
         setReportPage(0);
     }, [selectedChannelId]);
 
-useEffect(() => {
-    const fetchReports = async () => {
-        try {
-            let response;
-            if (selectedChannelId) {
-                response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/report/channelId`, {
-                    params: { channelId: Number(selectedChannelId) },
-                    withCredentials: true,
-                });
-            } else {
-                response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/report/all`, { withCredentials: true });
+    useEffect(() => {
+        const fetchReports = async () => {
+            try {
+                let response;
+                if (selectedChannelId) {
+                    response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/report/channelId`, {
+                        params: {channelId: Number(selectedChannelId)},
+                        withCredentials: true,
+                    });
+                } else {
+                    response = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/report/all`, {withCredentials: true});
+                }
+                const sortedReports = response.data.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
+                setReports(sortedReports);
+            } catch (err) {
+                console.error("Error fetching reports:", err);
+                setReports([]);
             }
-            const sortedReports = response.data.sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp));
-            setReports(sortedReports);
-        } catch (err) {
-            console.error("Error fetching reports:", err);
-            setReports([]);
-        }
-    };
-    fetchReports();
-}, [selectedChannelId]);
+        };
+        fetchReports();
+    }, [selectedChannelId]);
 
     return (
         <div className="ai-chat-page">
@@ -60,7 +61,8 @@ useEffect(() => {
                 {/*<header className="ai-chat-header">*/}
                 {/*    <h1>AI 리포트</h1>*/}
                 {/*</header>*/}
-                <ToolTip title="AI 리포트" tooltipText="지성이가 텍스트를 알려줍니다." />
+                <ToolTip title="AI 리포트" tooltipText="모니터링 중인 텔레그램 채널에서 채팅이 발생하면 그 채팅을 AI가 분석하여 리포트 형태로 누적합니다.
+                        기본적으로는 모든 채널에서 분석된 리포트가 최신순으로 보여지고 왼쪽에서 특정 채널을 클릭하면 해당 채널의 분석 리포트를 조회할 수 있습니다."/>
                 <div className="ai-chat-content">
                     <div className="chatbot-list">
                         <h3 className="tooltip" data-tooltip="현재 active 상태인 텔레그램 채널을 표시합니다.">텔레그램 채널</h3>
